29e63a6d07674075d537afe8905325eacbe5d0b6
Ian Chege <ianchege001@gmail.com>
2023-05-14 20:44:31 +0300

    Envs added

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ const image = require('./controllers/image');
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'test',
-      database : 'smart-brain'
+      connectionString : process.env.DATABASE_URL,
+      host : process.env.DATABASE_HOST,
+      ssl : {rejectUnauthorized:false},
+      port : 5432,
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PW,
+      database : process.env.DATABASE_DB
     }
   });
 
